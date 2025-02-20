b148984cb3feb2927b365e1d042b9783627bc51d
Jenn Salazar <jenniferdayanasr@gmail.com>
2025-02-20 15:03:22 -0500

    iniciando creación del simulador

--- src/routes/simulador/$estructura.tsx
+++ src/routes/simulador/$estructura.tsx
@@ -5,5 +5,28 @@ export const Route = createFileRoute("/simulador/$estructura")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/simulador/$estructura"!</div>;
+  return (
+    <div className="h-screen flex flex-col">
+      <div>
+        <h1 className="font-bold text-3xl text-center mt-2">NOMBRE ESTRUCTURA</h1>
+      </div>
+      <div className="flex-1 bg-gray-200 mx-5 my-3 flex flex-col rounded-xl">
+        <div className="flex-[2] flex flex-row rounded-xl my-3 mx-3">
+          <div className="flex-[2] bg-blue-400 rounded-3xl mr-2">
+
+          </div>
+          <div className="flex-[1] bg-blue-200 rounded-xl ml-2">
+          </div>
+        </div>
+        <div className="flex-[1] flex flex-row rounded-xl mb-3 mx-3">
+          <div className=" flex-1 bg-blue-300 mr-2 rounded-xl">
+
+          </div>
+          <div className="flex-1 bg-blue-500 ml-2 rounded-xl">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
