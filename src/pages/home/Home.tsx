ce900a4ece6a73317c5c0f0a4e77b7ed2723f2c1
Jenn Salazar <jenniferdayanasr@gmail.com>
2025-03-25 17:05:45 -0500

    Mejorando el home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CardList } from "./components/CardList";
 import { NavBar } from "./components/NavBar";
 import { FilterState } from "../../types";
 import { data } from "../../shared/constants/data-cards";
-import { motion } from "framer-motion";
+import { Welcome } from "./components/Welcome";
+import { Filters } from "./components/Filters";
 
 export function Home() {
     const [filter, setFilter] = useState<FilterState>({
@@ -11,79 +12,47 @@ export function Home() {
         type: "none",
     });
 
+    const [showButton, setShowButton] = useState(false);
+
+    // Detectar scroll para mostrar/ocultar el botón
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 300) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
     return (
-        <div className="w-full m-auto bg-[#efeff0] min-h-screen">
-            <NavBar filter={filter} setFilter={setFilter} />
-            <div className="w-full bg-white">
-                <div className="max-w-[1200px] mx-auto px-6 py-4 w-full bg-white border-rounded grid grid-cols-3 gap-4 items-center">
-                    {/* Sección de Presentación */}
-                    <div className="col-span-2">
-                        <p className="text-5xl font-bold">
-                            Bienvenidos al Mediador Pedagógico sobre
-                        </p>
-                        <p className="text-5xl font-bold text-red-600">
-                            Estructuras de Datos
-                        </p>
-
-                        <div className="rounded-lg mb-8 mt-6">
-                            <p className="text-lg mb-4">
-                                Aquí podrás encontrar dos tipos de simuladores:
-                            </p>
-
-                            <div className="mb-6">
-                                <h2 className="text-xl font-semibold text-red-600 mb-2">
-                                    Simulador de Memoria
-                                </h2>
-                                <p>
-                                    En este simulador aprenderás cómo se manejan
-                                    las direcciones de memoria para todas las
-                                    variables existentes en Java, permitiéndote
-                                    visualizar cómo se asigna, utiliza y libera
-                                    la memoria en tiempo real.
-                                </p>
-                                <motion.button
-                                    style={{ backgroundColor: "#D02222" }}
-                                    className="rounded text-white py-2 px-2 text-center mt-4"
-                                    whileHover={{ scale: 1.1 }}
-                                    whileTap={{ scale: 0.9 }}
-                                    transition={{
-                                        type: "spring",
-                                        stiffness: 300,
-                                        damping: 10,
-                                    }}
-                                    onClick={() => {}}
-                                >
-                                    Ver Simulador
-                                </motion.button>
-                            </div>
-
-                            <div>
-                                <h2 className="text-xl font-semibold text-red-600 mb-2">
-                                    Simuladores de Estructuras de Datos
-                                </h2>
-                                <p>
-                                    Este simulador abarca estructuras de datos
-                                    lineales, árboles binarios y árboles
-                                    n-arios. Podrás aprender cómo funcionan sus
-                                    operaciones más comunes y cómo se almacenan
-                                    y manipulan en la memoria, a continuación
-                                    las estructuras de datos disponibles:
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Imagen del Logo */}
-                    <div className="flex justify-center items-center">
-                        <img
-                            className="w-4/4 h-auto"
-                            src="/assets/images/logo_ingsistemas.png"
-                            alt=""
-                        />
-                    </div>
-                </div>
-            </div>
+        <>
+            <NavBar />
+            <Welcome />
+            <Filters filter={filter} setFilter={setFilter} />
             <CardList data={data} filter={filter} />
-        </div>
-    );
+            {/* Botón de volver arriba */}
+            {showButton && (
+                <button
+                    aria-label="Arrow Up"
+                    id="arrowUp"
+                    className="fixed bottom-4 right-4 w-12 h-12 bg-black text-white rounded-full shadow-lg flex items-center justify-center hover:bg-red-700 transition-all"
+                    onClick={scrollToTop}
+                >
+                    <i className="pi pi-chevron-circle-up text-2xl"></i>
+                </button>
+            )}
+        </>
+    )
 }
--- src/pages/home/components/NavBar.tsx
+++ src/pages/home/components/NavBar.tsx
@@ -1,8 +1,13 @@
-import { TiDelete } from "react-icons/ti";
-import { FilterTypeValue, NavBarProps } from "../../../types";
-import { TYPE_FILTER } from "../../../shared/constants/consts";
+export function NavBar() {
+    const handleScroll = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault(); // Evita el comportamiento por defecto del enlace
 
-export function NavBar({ filter, setFilter }: NavBarProps) {
+        const filtersSection = document.getElementById("filters-section");
+        if (filtersSection) {
+            filtersSection.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+    };
+
     return (
         <header className="sticky top-0 w-full z-50 bg-white shadow">
             <div className="m-auto flex flex-col sm:flex-row sm:flex items-center sm:justify-between px-6 w-full  max-w-[1200px] bg-white h-32 sm:h-24 gap-2 sm:gap-10">
@@ -21,52 +26,13 @@ export function NavBar({ filter, setFilter }: NavBarProps) {
                     <span className="text-black">Mediador</span>
                     <span className="text-red-600">Pedagógico</span>
                 </h1>
-                <div className="flex min-w-64 lg:w-64 items-center bg-white p-1 border border-red-300 rounded-full overflow-hidden">
-                    <input
-                        type="text"
-                        className="w-52 sm:w-52 px-2 lg:w-64 outline-none"
-                        value={filter.query}
-                        onChange={(e) =>
-                            setFilter((prev) => ({
-                                ...prev,
-                                query: e.target.value,
-                            }))
-                        }
-                        placeholder="Secuencia, Cola, Lista..."
-                    />
-                    <TiDelete
-                        className="cursor-pointer rounded-full text-red-500"
-                        size={25}
-                        onClick={() =>
-                            setFilter((prev) => ({ ...prev, query: "" }))
-                        }
-                    />
-                </div>
-                <div className="relative w-60 md:w-64 lg:w-72">
-                    <select
-                        onChange={(e) =>
-                            setFilter((prev) => ({
-                                ...prev,
-                                type: e.target.value as FilterTypeValue,
-                            }))
-                        }
-                        className="w-60 md:w-64 lg:w-72 p-1 border rounded border border-red-300 outline-none appearance-none"
-                    >
-                        <option value={TYPE_FILTER.NONE}>Por defecto</option>
-                        <option value={TYPE_FILTER.ESTRUCTURA_LINEAL}>
-                            Estructuras Lineales
-                        </option>
-                        <option value={TYPE_FILTER.ARBOL_BINARIO}>
-                            Árboles Binarios
-                        </option>
-                        <option value={TYPE_FILTER.ARBOL_ENEARIO}>
-                            Árboles Enearios
-                        </option>
-                    </select>
-                    <div className="absolute inset-y-0 right-2 flex items-center pointer-events-none">
-                        <i className="pi pi-chevron-down text-red-500"></i>
-                    </div>
-                </div>
+                <a
+                    href="#filters-section"
+                    className="text-lg font-semibold leading-6 "
+                    onClick={handleScroll}
+                >
+                    Ver Estructuras de Datos <span aria-hidden="true">&rarr;</span>
+                </a>
             </div>
         </header>
     );
