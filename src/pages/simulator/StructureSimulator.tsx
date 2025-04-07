890217a47af1c94c2d6439df15e2b581f291d4d9
EfranorQM <andersonefrainquma@gmail.com>
2025-04-06 21:37:28 -0500

    Arreglando errores del loco de Jaider

--- src/pages/simulator/StructureSimulator.tsx
+++ src/pages/simulator/StructureSimulator.tsx
@@ -1,14 +1,13 @@
 import { getRouteApi } from "@tanstack/react-router";
 import { conceptosData } from "../../shared/constants/conceptsData";
 import { SequenceSimulator } from "./SequenceSimulator";
-import { StackSimulator } from "./StackSimulator";
-import { QueueSimulator } from "./QueueSimulator";
+
 
 // Mapea los nombres a sus respectivos componentes
 const componentMap: Record<string, React.FC> = {
     Secuencia: SequenceSimulator, // Si nombre = "Secuencia", renderiza <SequenceSimulator />
-    Cola: QueueSimulator,
-    Pila: StackSimulator,
+    // Cola: QueueSimulator,
+    // Pila: StackSimulator,
 };
 
 export function StructureSimulator() {
